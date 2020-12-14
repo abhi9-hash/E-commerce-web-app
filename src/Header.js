fd0718b30b5e9b2cc9e106b7a90b39a3bbf19a59
abhnv_pthk <coolabhinav57@gmail.com>
2020-12-15 01:51:19 +0530

    home page setup

---
+++ src/Header.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import './Header.css'
+import SearchIcon from '@material-ui/icons/Search'
+function Header() {
+    return (
+        <div className='header'>
+            <div  className='header_logo'>E-COM 🛒</div>
+            <div className='header_search'>
+                <input className='header_search_input' placeholder='search'  type='text'></input>
+                <SearchIcon className='header_search_icon'/>
+            </div>
+            <div className='header_nav'>
+                <div className='header_nav_option1'>
+                <div>
+                     <span > Hello Guest</span>
+                    </div>
+                    <div>
+                    <span > SignIn</span>
+                    </div>
+            </div>
+                <div className='header_nav_option2'>
+                    <span> orders</span>
+                </div>
+            </div>
+            <div className='header_cart'>
+                    <h2>🛒 0</h2>
+                </div>
+        </div>
+        
+
+    )
+}
+
+export default Header
