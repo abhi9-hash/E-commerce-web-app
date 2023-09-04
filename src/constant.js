f1acbceb6fce2952a981a2c7e164b441c8ceae1d
ABHINAV PATHAK <62811014+abhi9-hash@users.noreply.github.com>
2023-09-05 02:08:17 +0530

    Update constant.js

--- src/constant.js
+++ src/constant.js
@@ -1,3 +1,3 @@
 // export const BASEURL="https://ekomm.herokuapp.com";
-export const BASEURL="https://e-commerce-web-app-production-2032.up.railway.app"
+export const BASEURL="https://e-comm-bnw8.onrender.com"
 export const CLIENTURL="https://buyfromhere.netlify.app";
